d3f37f857e8ae0517d3115ecff0413d2b9d64dd4
Catalan-Nick <catalan1222@yahoo.com>
2022-07-27 15:02:45 -0500

    Added Views

--- feed/models.py
+++ feed/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
 class Post(models.Model):
-    pass
+    text = models.CharField(max_length=140, blank=False, null=False)
+    
+    def __str__(self):
+        return self.text
